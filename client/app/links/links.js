b7a97303edd60e5fb3704e87eadc4b6da1b63e08
mwharris333 <mwharris333@gmail.com>
2015-04-23 10:55:05 -0700

    Made links template work with links controller, displays link "dummy data" from controller to view

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,8 +3,23 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   console.log('***************** LINKS.data:', Links.data)
+//  $scope.str = 'test';
   $scope.data = {}; // EXPERIMENT
-  $scope.data.links = [{}, {}, {}]; // NOTE TO SELF: figure out how to fill this with non-hard-coded values.
+  $scope.data.links = [{
+    name: 'somesite1',
+    origUrl: 'http://www.google.com',
+    shortenedUrl: 'someroute3'
+  }, // TODO: figure out official format of data
+  {
+    name: 'somesite2',
+    origUrl: 'http://www.somesite2.com',
+    shortenedUrl: 'someroute3'
+  },
+  {
+    name: 'somesite3',
+    origUrl: 'http://www.somesite3.com',
+    shortenedUrl: 'someroute3'
+  }]; // NOTE TO SELF: figure out how to fill this with non-hard-coded values.
   $scope.getLinks = Links.getLinks;
   $scope.getLinks(); // EXPERIMENT
 
